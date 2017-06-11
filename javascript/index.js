b30dc295e881af82c0847660c2a7e3deba8eba33
mitchpeh <mitchmac@Mitchells-MacBook-Pro.local>
2017-06-11 14:46:09 -0400

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $('footer').mouseenter(function() {
+      $("footer").css('background-color','rgba(235, 167, 49, 0.75)');
+  });
+
+
+  $('footer').mouseleave(function() {
+      $('footer').css('background-color','rgba(235, 167, 49, 0.5)');
+  });
+
+});
