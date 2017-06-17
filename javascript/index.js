f6a3bd2fc59dc9f222e535266ad366d7fe7a6ed3
mitchpeh <mitchmac@Mitchells-MacBook-Pro.local>
2017-06-17 10:56:38 -0400

    added mobile page

--- javascript/index.js
+++ javascript/index.js
@@ -10,3 +10,44 @@ $(document).ready(function() {
   });
 
 });
+
+var deviceIphone = "iphone";
+var deviceIpod = "ipod";
+
+//Initialize our user agent string to lower case.
+var uagent = navigator.userAgent.toLowerCase();
+
+//**************************
+// Detects if the current device is an iPhone.
+function DetectIphone()
+{
+   if (uagent.search(deviceIphone) > -1)
+      return true;
+   else
+      return false;
+}
+
+//**************************
+// Detects if the current device is an iPod Touch.
+function DetectIpod()
+{
+   if (uagent.search(deviceIpod) > -1)
+      return true;
+   else
+      return false;
+}
+
+//**************************
+// Detects if the current device is an iPhone or iPod Touch.
+function DetectIphoneOrIpod()
+{
+    if (DetectIphone())
+       return true;
+       window.location.replace("file:///Users/mitchmac/Documents/github/harvest/mobiletest.html");
+    else if (DetectIpod())
+       return true;
+       window.location.replace("file:///Users/mitchmac/Documents/github/harvest/mobiletest.html");
+    else
+       return false;
+}
+console.log(DetectIpod)
