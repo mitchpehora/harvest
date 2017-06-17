59ea74dd2e5b69afdad7917f38b99734d6efcfa5
mitchpeh <mitchmac@Mitchells-MacBook-Pro.local>
2017-06-17 11:53:28 -0400

    buttons mobile
    added content to mobile page

--- javascript/index.js
+++ javascript/index.js
@@ -51,5 +51,5 @@ function DetectIphoneOrIpod()
 
 if (DetectIphoneOrIpod() ==true )
 {
-  window.location.replace("https://mitchpehora.github.io/harvest/mobiletest.html");
+  window.location.replace("https://mitchpehora.github.io/harvest/mobile.html");
 }
